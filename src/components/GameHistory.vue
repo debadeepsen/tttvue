9d2cb96bb7be17cab079d78d5fc7d833d7cc59df
Debadeep Sen <dsen@msi.home>
2024-01-21 00:11:46 +0530

    changes to history

--- src/components/GameHistory.vue
+++ src/components/GameHistory.vue
@@ -2,46 +2,33 @@
 import { ref } from 'vue'
 import { useGameStore } from '@/stores/game'
 import WaitingSymbol from './WaitingSymbol.vue'
-import { MOVE } from '@/utils/constants'
+import { CIRCLE, CROSS, MOVE } from '@/utils/constants'
 
-const gameStore = useGameStore()
-
+const store = useGameStore()
 </script>
 
 <template>
-  {{ gameStore.history }}
-  <!--
-  <table>
-    <tr>
-      <th>{{ gameStore.players?.[0] }}</th>
-      <th>{{ gameStore.players?.[1] }}</th>
-    </tr>
-    <tr v-for="([playerX, playerO], i) in gameStore.history" :key="i">
-      <td>
-        {{ getMove(playerX) }}
-        <waiting-symbol v-show="isCurrentMove(playerX)" />
-      </td>
-      <td>
-        {{ getMove(playerO) }}
-        <waiting-symbol v-show="isCurrentMove(playerO)" />
-      </td>
-    </tr>
-  </table>
--->
+  <div class="flex flex-wrap w-full shadow-md">
+    <div class="w-1/2">
+      <div class="col-header">{{ store.players?.[0] }}</div>
+      <div v-for="(move, i) in store.history[CROSS]" :key="i" class="row">
+        {{ move }}
+      </div>
+    </div>
+    <div class="w-1/2">
+      <div class="col-header">{{ store.players?.[1] }}</div>
+      <div v-for="(move, i) in store.history[CIRCLE]" :key="i" class="row">
+        {{ move }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-table {
-  @apply w-full border border-solid border-zinc-800/10;
+.col-header {
+  @apply w-full box-border bg-zinc-300 font-bold pl-2 py-2;
 }
-th,
-td {
-  @apply p-2 border-zinc-400 w-1/2;
-}
-tr:nth-child(2n + 1) {
-  @apply bg-zinc-400/10;
-}
-th {
-  @apply bg-zinc-300;
+.row {
+  @apply w-1/2 pl-2 py-2;
 }
 </style>
