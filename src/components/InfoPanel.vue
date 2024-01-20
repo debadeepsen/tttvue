a59f991c9d1fc4c0755915ac78d40c65efdb84d6
Debadeep Sen <dsen@msi.home>
2024-01-20 14:50:03 +0530

    refactoring and styling

--- src/components/InfoPanel.vue
+++ src/components/InfoPanel.vue
@@ -1,46 +1,32 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, toRefs } from 'vue'
 import { useGameStore } from '@/stores/game'
 import PlayerSymbol from '@/components/PlayerSymbol.vue'
+import PlayerNamesForm from '@/components/PlayerNamesForm.vue'
 import GameHistory from '@/components/GameHistory.vue'
 
-const players = ref(['Manuel', 'Pratyush'])
 const gameStore = useGameStore()
 
 const gameInProgress = computed(() => gameStore.started)
+const [playerX, playerO] = toRefs(gameStore.players)
 
-function startGame() {
-  gameStore.players = players.value
-  gameStore.started = true
-}
 </script>
 <template>
   <div class="game-container px-8 lg:px-16 min-h-full w-full lg:w-2/3">
     <h1 class="mb-6">Tic-Tac-Toe</h1>
     <div v-show="!gameInProgress" class="border border-zinc-400 w-full p-2">
-      <h3 class="mt-8 lg:mt-0 mb-4">Enter the names of the players</h3>
-      <div class="flex">
-        <input type="text" placeholder="Player 1" v-model="players[0]" />
-        <player-symbol icon="cross" icon-class="mt-2" />
-      </div>
-      <div class="mt-2 flex">
-        <input type="text" placeholder="Player 2" v-model="players[1]" />
-        <player-symbol icon="circle" icon-class="mt-2" />
-      </div>
-      <div class="mt-2">
-        <button class="primary" @click="startGame">Start Game</button>
-      </div>
+      <player-names-form />
     </div>
     <div v-show="gameInProgress">
       <div>
         Game between
         <div class="player-name bg-red-400/20 text-red-600">
-          {{ gameStore.players[0] }}
+          {{ playerX }}
           <player-symbol icon="cross" />
         </div>
         and
         <div class="player-name bg-blue-400/20 text-blue-600">
-          {{ gameStore.players[1] }}
+          {{ playerO }}
           <player-symbol icon="circle" />
         </div>
       </div>
---
+++ src/components/PlayerNamesForm.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useGameStore } from '@/stores/game'
+import PlayerSymbol from '@/components/PlayerSymbol.vue'
+
+const players = ref(['Manuel', 'Pratyush'])
+const gameStore = useGameStore()
+
+function startGame() {
+  gameStore.players = players.value
+  gameStore.started = true
+}
+</script>
+
+<template>
+  <h3 class="mt-8 lg:mt-0 mb-4">Enter the names of the players</h3>
+  <div class="player-name">
+    <player-symbol icon="cross" />
+    <input type="text" placeholder="Player 1" v-model="players[0]" />
+  </div>
+  <div class="mt-2 player-name">
+    <player-symbol icon="circle" />
+    <input type="text" placeholder="Player 2" v-model="players[1]" />
+  </div>
+  <div class="mt-2">
+    <button class="primary" @click="startGame">Start Game</button>
+  </div>
+</template>
+
+<style scoped>
+input[type='text'] {
+  @apply w-full border-none text-base outline-0;
+}
+.player-name {
+  @apply pl-2 flex items-center rounded-sm transition-all;
+  box-shadow: 0px 0px 0px 1px #1112;
+}
+div:has(input:focus) {
+  box-shadow: 0px 0px 0px 1px #00acc1;
+}
+</style>
